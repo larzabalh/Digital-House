199f702dbb4b7f4929977dddb4f7978857b07353
marianoberton <marianoberton@gmail.com>
2017-10-11 12:04:30 -0300

    11 Oct

---
+++ PHP/Clase-13/prueba.php
@@ -0,0 +1,50 @@
+<?php
+
+require 'usuario.php';
+
+// $hernan = new Usuario();
+// $hernan -> nombre = 'HERNAN';
+// $hernan -> mail = 'larzabalh@hotmail.com';
+// $hernan -> fecha= '30/12/1980';
+
+// echo "Datos de Usuario 1: {$hernan->nombre}, mail: {$hernan->mail} y fecha de nacimiento: {$hernan->fecha}";
+
+$gisela =new Usuario('Gisela','gisela@gmail.com','22/01/1979',38);
+// echo "Datos de Usuario 2: {$gisela->nombre}, mail: {$gisela->mail} y fecha de nacimiento: {$gisela->fecha}"."<br>";
+
+$omar =new Usuario('Omar','omar@gmail.com','24/03/1951',66);
+// echo "Datos de Usuario 3: {$omar->nombre}, mail: {$omar->mail} y fecha de nacimiento: {$omar->fecha}"."<br>";
+
+// $gisela ->mail ='cambiomail@gmail.com';
+// echo "Datos de Usuario 2: {$gisela->nombre}, mail: {$gisela->mail} y fecha de nacimiento: {$gisela->fecha}"."<br>";
+
+var_dump($gisela);
+echo "<br>";
+
+var_dump($omar);
+echo "<br>";
+
+echo $gisela->getNombre();
+echo "<br>";
+
+echo $gisela->getMail();
+echo "<br>";
+
+echo $gisela->getFecha();
+echo "<br>";
+
+echo $gisela->setNombre('loca');
+echo "<br>";
+echo $gisela->getNombre();
+echo "<br>";
+
+echo $gisela->setMail('loca@gmail.com');
+echo "<br>";
+
+echo $gisela->setEdad(30.2);
+echo "<br>";
+
+
+
+
+ ?>
---
+++ PHP/Clase-13/usuario.php
@@ -0,0 +1,68 @@
+<?php
+class Usuario{
+
+  private $nombre;
+  private $mail;
+  private $fecha;
+  private $edad;
+
+  function __construct($nombre,$mail,$fecha,int $edad)
+  {
+    $this-> nombre = $nombre;
+    $this-> mail =$mail;
+    $this-> fecha =$fecha;
+    $this-> edad =$edad;
+  }
+
+  public function saludar(){
+    return $this-> nombre;
+
+  }
+
+  public function getNombre(){
+    return $this->nombre;
+  }
+
+  public function getMail(){
+    return $this->mail;
+  }
+
+  public function getFecha(){
+    return $this->fecha;
+  }
+
+  public function getEdad(){
+    return $this->edad;
+  }
+
+  public function setNombre($nombre){
+    return $this->nombre=$nombre;
+  }
+  public function setMail($mail){
+
+    if (filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        echo "Esta dirección de correo ($mail) es válida.";
+        $this->mail=$mail;
+    }
+
+
+  }
+  public function setEdad($edad){
+
+if (is_int($edad)) {
+  $this->edad = $edad;
+  Echo "se pudo modificar la edad a su nuevo valor ".$edad;
+}else {
+  Echo "No se puede modificar la edad, porque ".$edad." no es un entero. La edad es:". $this->edad;
+}
+
+  }
+
+
+}
+
+
+
+
+
+ ?>
